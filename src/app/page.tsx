d41d80614d6ef5b320387e7b63015a202418c8eb
Tan Bunchhay <bunchay1280@gmail.com>
2024-08-15 11:08:03 +0700

    update element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 
+
+
+
 const page = () => {
   return (
     <>
       <div className="flex flex-row gap-6">
         <div>
-          <Dashboard children={undefined}></Dashboard>
+
         </div>
       </div>
     </>
